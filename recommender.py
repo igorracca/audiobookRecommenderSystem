5ce8830b9d3a51342b9dfe591e16a67e94c7fc5b
igorracca <igorracca13@gmail.com>
2019-11-27 21:05:12 +0100

    adding normalization

--- recommender.py
+++ recommender.py
@@ -8,9 +8,15 @@ DEBUG = True
 
 # defining rating class
 class Rating:
-	def __init__(self, audiobook, rating):
-		self.a = audiobook
+	def __init__(self, rating, normalized):
 		self.r = rating
+		self.n = normalized
+
+# defining userSimilarity
+class Similarity:
+	def __init__(self, user, similarity):
+		self.u = user
+		self.s = similarity
 
 # defining print function for debug mode
 def printDebug(s):
@@ -19,10 +25,29 @@ def printDebug(s):
 # printing ratings matrix #DEBUG
 def printRatingsDebug():
 	if DEBUG:
-		for row in ratings:
-			for val in row:
-				print('a[%s]:%s' % (val.a, val.r), end = '\t')
+		for userRatings in ratings:
+			for rating in userRatings:
+				print(rating.r, end = '\t')
 			print()
+	print()
+
+# printing normalized ratings matrix #DEBUG
+def printNormalizedRatingsDebug():
+	if DEBUG:
+		for userRatings in ratings:
+			for rating in userRatings:
+				print(rating.n, end = '\t')
+			print()
+	print()
+
+# printing similarity matrix #DEBUG
+def printSimilarityDebug():
+	if DEBUG:
+		for userSimilarities in similarities:
+			for similarity in userSimilarities:
+				print('s[%s]:%s' % (similarity.u, similarity.s), end = '\t')
+			print()
+	print()
 
 ####################################################################################################################
 # Main
@@ -31,17 +56,34 @@ def printRatingsDebug():
 # reading R, U and A size (ratings, users and audiobooks)
 R, U, A = map(int, input().split())
 
-# initializang list of user ratings
-ratings = [[] for i in range(U)]
+# initializang ratings matrix
+ratings = [[Rating(0,0)]*A for i in range(U)]
 
+j = 0
 for i in range(R):
 	# reading recommendations from stdin
 	u, a, r = map(int, input().split())
-	printDebug([u,a,r])
-	# creating obj rating
-	rating = Rating(a,r)
-	# appending to the ratings list according to user u
-	ratings[u].append(rating)
-
+	if(u != j):
+		j+=1
+	# printDebug([u,j,a,r])
+	ratings[j][a] = Rating(r,0)
 printRatingsDebug()
 
+# normalize ratings
+for i in range(0, len(ratings)):
+	# calculate the row mean 
+	rowMean = sum(x.r for x in ratings[i]) / sum(1 for x in ratings[i] if x.r != 0)
+	# printDebug('rowMean: %s' % rowMean)
+	for j in range(0, len(ratings[i])):
+		if(ratings[i][j].r == 0):
+			ratings[i][j].n = 0
+		else:
+			ratings[i][j].n = ratings[i][j].r - rowMean
+printNormalizedRatingsDebug()
+
+# # initializing user similary matrix
+similarities = []
+
+# printSimilarityDebug()	
+	
+		
